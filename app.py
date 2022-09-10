1c6bc5139f63103524202d7dae304ca69df0ef04
Ayanarmani <98839320+Ayanarmani@users.noreply.github.com>
2022-09-10 12:31:41 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import numpy as np
 import pickle
-import streamlit
+import streamlit as st
 
 #Loading the saved model
 loaded_model = pickle.load(open('C:/Users/AYANAR/Desktop/Deployment/trained-model.pkl','rb'))
@@ -9,7 +9,7 @@ loaded_model = pickle.load(open('C:/Users/AYANAR/Desktop/Deployment/trained-mode
 def heart_disease_prediction(input_data):
 
     #changing the input into numpy array
-    input_data_as_numpy_array = np.asarray(input_data)
+    input_data_as_numpy_array = np.array(input_data, dtype=float)
 
     #reshaping the array as we are predicting for one instance
     input_data_reshaped = input_data_as_numpy_array.reshape(1,-1)
@@ -18,9 +18,9 @@ def heart_disease_prediction(input_data):
     print(prediction)
 
     if(prediction[0]==0):
-        return "The Person don't have heart disease"
+        return "This Person don't have heart disease"
     else:
-        return "The Person has heart disease! Better consult with doctor"
+        return "This Person has heart disease! Better consult with doctor"
 
 def main():
 
@@ -29,15 +29,15 @@ def main():
 
     #getting the input data from the user
     age = st.text_input("Age")
-    sex = st.text_input("Gender")
-    cp = st.text_input("Chest Pain eg: 0-Typical angina, 1-Atypical angina, 2-Non-anginal, 3-Asymptomatic")
-    fbs = st.text_input("Fasting blood sugar eg:0-False, 1-True")
-    restecg = st.text_input("ECG result eg: 0-nothing, 1:ST-T Abnormality, 2:Possible Hypertrophy")
-    exang = st.text_input("Exercise induced angina eg: 1=yes; 0 = no")
+    sex = st.text_input("Gender (1-male: 0-female)")
+    cp = st.text_input("Chest Pain (eg: 0-Typical angina, 1-Atypical angina, 2-Non-anginal, 3-Asymptomatic)")
+    fbs = st.text_input("Fasting blood sugar > 120mg/dl (eg:0-False, 1-True)")
+    restecg = st.text_input("Resting ECG result (eg: 0-nothing, 1:ST-T Abnormality, 2:Possible Hypertrophy)")
+    exang = st.text_input("Exercise induced angina (eg: 1=yes; 0 = no)")
     oldpeak = st.text_input("ST depression value")
-    slope = st.text_input("Slopping eg:0-upslopping, 1-flatslopping, 2-downslopping")
-    ca = st.text_input("No.of Vessels (0-3) coloured by flourosopy")
-    thal = st.text_input("Thalium stress eg: 1-3:Normal, 6-fixed defect, 7-reversable defect")
+    slope = st.text_input("Slopping (eg:0-upslopping, 1-flatslopping, 2-downslopping)")
+    ca = st.text_input("No.of Vessels coloured by flourosopy (eg:0-3")
+    thal = st.text_input("Thalium stress level (eg: 1-3:Normal, 6-fixed defect, 7-reversable defect)")
 
     #code for prediction
     diagnosis = ""
